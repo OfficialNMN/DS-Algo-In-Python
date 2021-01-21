395cb417ebc88f59e46782127697026c0f16a814
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-01-21 18:31:10 +0530

    Update Using Collections module.py

--- Queue.py/Using Collections module.py
+++ Queue.py/Using Collections module.py
@@ -1,1 +1,43 @@
-ee
+''' Queue using Deque of collections '''
+
+from collections import deque
+
+class Queue:
+	def __init__(self,maxsize):
+		self.queue=deque()
+		self.maxsize=maxsize
+
+	def enqueue(self,value):
+		if self.size()==self.maxsize:
+			print('OverflowError')
+		else:
+			self.queue.append(value)
+
+	def dequeue(self):
+		if self.isEmpty():
+			print('UnderflowError')
+		else:
+			self.queue.popleft()
+
+	def traverse(self):
+		return self.queue
+
+	def size(self):
+		return len(self.queue)
+
+	def peek(self):
+		return self.queue[0]
+
+	def isEmpty(self):
+		return len(self.queue)==0
+
+
+q=Queue(10)
+print(q.isEmpty())
+q.enqueue(5)
+q.enqueue(6)
+q.enqueue(7)
+q.dequeue()
+print(q.peek())
+print(q.traverse())
+q.dequeue()
