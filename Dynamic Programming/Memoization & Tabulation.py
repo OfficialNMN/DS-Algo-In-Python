e12ee15c40ae173c6789ab233ba58f296130bc17
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-01-28 14:06:19 +0530

    Create Memoization & Tabulation.py

---
+++ Dynamic Programming/Memoization & Tabulation.py
@@ -0,0 +1,25 @@
+'''Memoization means to store the value of a particular case and use it again later in program when needed to avoid the computation time again '''
+
+''' Top Down Approach(Recursive) focuses on breaking down a big problem into smaller and understandable chunks '''
+
+def Memofib(n,memo):
+	if n==1:
+		return 0
+	if n==2:
+		return 1
+	if not n in memo:
+		memo[n]=Memofib(n-1,memo)+Memofib(n-2,memo)
+	return memo[n]
+
+mydict={}
+print(Memofib(5,mydict))
+
+''' Bottom Up Approach(Tabulation) first focuses on solving the smaller problems at the fundamental level and then integrating them into a whole and complete solution '''
+
+def fibTab(n):
+	tb=[0,1]
+	for i in range(2,n+1):
+		tb.append(tb[i-1]+tb[i-2])
+	return tb[n-1]
+	
+print(fibTab(5))
