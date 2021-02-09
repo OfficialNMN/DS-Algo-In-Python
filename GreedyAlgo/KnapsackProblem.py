cb239884969f2e50e71adab03a845cbddc32b5fe
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-02-10 00:46:45 +0530

    Delete KnapsackProblem.py

--- GreedyAlgo/KnapsackProblem.py
+++
@@ -1,6 +0,0 @@
-#1.Knapsack Problem----> A thief enters a house to steal items with maximum worth within a limit of k items only in the bag.
-
-def items(cost,k):
-    cost.sort()
-    cost.reverse()             # reversed the sorted array to get k largest items
-    return sum(cost[:k])
