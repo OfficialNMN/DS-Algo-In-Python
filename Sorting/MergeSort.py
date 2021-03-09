8a2ae9cbf5a6fdc3e8d12b8053c13b8ef2ffc569
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-03-09 13:54:44 +0530

    Add files via upload

---
+++ Sorting/MergeSort.py
@@ -0,0 +1,38 @@
+def merge(a1,a2,a):
+	i=0
+	j=0
+	k=0
+	while i < len(a1) and j < len(a2):
+		if (a1[i] < a2[j]):
+			a[k] = a1[i]
+			k+=1
+			i+=1
+		else:
+			a[k] = a2[j]
+			k+=1
+			j+=1
+
+	# To fill the leftover elements of the longer list		
+	while i < len(a1):
+		a[k]=a1[i]
+		k+=1
+		i+=1
+
+	while j < len(a2):
+		a[k] = a2[j]
+		k+=1
+		j+=1		
+
+def mergesort(a):
+	if len(a)==0 or len(a)==1:
+		return 
+	mid=len(a)//2
+	a1 = a[0:mid]
+	a2 = a[mid:]
+	mergesort(a1)
+	mergesort(a2)
+	merge(a1,a2,a)
+
+a=[10,9,2,4,6,13]
+mergesort(a)
+print(a)
