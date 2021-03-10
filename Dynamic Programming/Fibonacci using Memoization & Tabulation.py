5200f9426d6392438f9c886b0461747e9c99edce
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-03-10 13:52:01 +0530

    Update Fibonacci using Memoization & Tabulation.py

--- Dynamic Programming/Fibonacci using Memoization & Tabulation.py
+++ Dynamic Programming/Fibonacci using Memoization & Tabulation.py
@@ -1,7 +1,8 @@
 '''Memoization means to store the value of a particular case and use it again later in program when needed to avoid the computation time again '''
 
-''' Top Down Approach(Recursive) focuses on breaking down a big problem into smaller and understandable chunks '''
+''' Top Down Approach(Recursive + Memoization) focuses on breaking down a big problem into smaller and understandable chunks '''
 
+# Time Complexity---->O(n)
 def fibonacci(n,memo):
 	if n==0 or n==1:
 		return n
@@ -15,6 +16,7 @@ print(fibonacci(9,my_dict))
 
 ''' Bottom Up Approach(Tabulation) first focuses on solving the smaller problems at the fundamental level and then integrating them into a whole and complete solution '''
 
+# Time Complexity---->O(n)
 def fibTab(n):
 	tb=[0,1]
 	for i in range(2,n+1):
