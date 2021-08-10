68da7f5cdce039758ab1b811fad887b955486eb9
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-08-10 19:35:43 +0530

    Update Kadane'sAlgo.py

--- Array/Kadane'sAlgo.py
+++ Array/Kadane'sAlgo.py
@@ -1,10 +1,16 @@
-def maxSubArraySum(a,size):
-	local_sum=0
-	max_sum=0
-	for i in range(size):
-		local_sum=max(a[i],a[i]+local_sum)
-		if local_sum>max_sum:
-			max_sum=local_sum
-	return max_sum
+def maxSubArraySum(nums):
+	# initializing local_sum = 0 and max_sum to nums[0]  
+    local_sum=0
+    max_sum=nums[0]
+    # iterate through the array and add nums[i] to local_sum
+    for i in range(len(nums)):
+        local_sum+=nums[i]
+        # if local > max then change max_sum to local_sum
+        if local_sum>max_sum:
+            max_sum=local_sum
+        # if local_sum < 0 we reset it to 0
+        if local_sum < 0:
+            local_sum=0
+    return max_sum
 
-print(maxSubArraySum([1,2,3,-2,5],5))
+print(maxSubArraySum([1,2,3,-2,5]))
