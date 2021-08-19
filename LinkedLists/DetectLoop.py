1813d30b9246c60352decead4a60a48f27614410
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-08-19 18:54:54 +0530

    Add files via upload

--- LinkedLists/DetectLoop.py
+++ LinkedLists/DetectLoop.py
@@ -1,12 +1,40 @@
-def detectloop(head):
-	slow=fast=head
-	while(slow and fast and fast.next):
-		slow=slow.next
-		fast=fast.next.next
-		# if both slow and fast meet we have a loop
-		if slow==fast:
-			return True
-	return False
+def hasCycle(head):
+    if head is None or head.next is None:
+        return None
+    slow=head
+    fast=head
+    # both fast and slow will meet if theres a list present
+    while(fast.next!=None and fast.next.next!=None ):
+        slow=slow.next
+        fast=fast.next.next
+        if fast==slow:
+            return True
+    return False
 
 # creating a loop to check 
 l.head.next.next.next.next = l.head    
+
+# Find starting point of Loop 
+# Using Floyd Cycle Detection Algorithm
+
+def detectCycleStart(head):
+    if head==None or head.next==None:
+        return None
+    slow=head
+    fast=head
+    entry=head
+    # to detect whether cycle is there or not
+    while(fast.next!=None and fast.next.next!=None):
+        slow=slow.next
+        fast=fast.next.next
+        if slow==fast:
+            # this means a cycle exist
+            # now we traverse both slow and entry to find the cycle point
+            while(slow!=entry):
+                slow=slow.next
+                entry=entry.next
+            # point where entry and slow collides is our answer
+            return entry
+    # no cycle present
+    return None
+
