a7eaf8181292c3e4d216bcb8b2ca50c628ec8e3d
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-09-06 19:21:50 +0530

    Update NextGreaterElement.py

--- Stack/NextGreaterElement.py
+++ Stack/NextGreaterElement.py
@@ -1,17 +1,23 @@
 # To find next greater element for each number in the given array
 
-def nextGreater(A):
-	greater=[]
-	for i in range(len(A)):
-		found=False
-		for j in range(i+1,len(A)):
-			if A[i]<A[j]:
-				found=True
-				greater.append(A[j])
-				break
-		if found==False:
-			greater.append(-1)
-
-	return greater
+def nextLargerElement(arr,n):
+    stack=[arr[-1]]
+    ans=[]
+    # startinf from behind and appending those to stack
+    for i in range(n-1,-1,-1):
+    	# if smaller element in stack then pop it out 
+        while(len(stack)!=0 and arr[i]>=stack[-1]):
+            stack.pop()
+        # if stack becomes empty -1 is the ans
+        # otherwise the top of stack 
+        if len(stack)==0:
+            ans.append(-1)
+        else:
+            ans.append(stack[-1])
+        # add every processed element to stack
+        stack.append(arr[i])
+    # reverse the ans since we traversed backwards
+    ans.reverse()
+    return ans
 
 print(nextGreater([4,5,2,10]))
