c872ec77d98fa0da2e0496d9867967cdb39ad7fd
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-11-03 17:42:46 +0530

    Update PrintStringPremutation.py

--- Recursion/PrintStringPremutation.py
+++ Recursion/PrintStringPremutation.py
@@ -1,13 +1,1 @@
-def printpermutations(ques,ans):
-    if (len(ques)==0):
-        print(ans)
-        return 
-    for i in range(len(ques)):
-        c=ques[i]
-        left=ques[:i]
-        right=ques[i+1:]
-        rest=''
-        rest=left+right
-        printpermutations(rest,ans+c)
 
-printpermutations('abc','')
