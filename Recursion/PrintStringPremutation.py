dead98972855229cefd398d41a5f24acf27064e7
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-11-09 00:00:36 +0530

    Update PrintStringPremutation.py

--- Recursion/PrintStringPremutation.py
+++ Recursion/PrintStringPremutation.py
@@ -1,1 +1,14 @@
 
+def permutation(s):
+    if len(s)==1:
+        return [s]
+    perms=permutation(s[1:])
+    char=s[0]
+    result=[]
+    for c in perms:
+        for i in range(len(c)+1):
+            result.append(c[:i]+char+c[i:])
+    result.sort()
+    return result
+
+print(permutation('ABC'))
