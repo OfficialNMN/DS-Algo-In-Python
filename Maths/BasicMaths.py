cbcce8d2f57db1ad9af8b41a55706e97a13b2b1e
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-11-20 14:47:20 +0530

    Update BasicMaths.py

--- Maths/BasicMaths.py
+++ Maths/BasicMaths.py
@@ -1,18 +1,20 @@
-# To find whether a number is prime or not
-# TimeComplexity--->-O(n)
+import numpy as np
 
+# To find whether a number is prime or not
+# TimeComplexity--->O(sqrt(n))
 def isprime(n):
-	for i in range(2,n):
+	for i in range(2,int(np.sqrt(n))+1):
 		if (n%i)==0:
 			return False
 	return True
 
 # TimeComplexity---->O(n*sqrt(n))
-
-def PrintAllPrime(n):
+def printPrime(n):
 	for i in range(2,n+1):
 		if (isprime(i)):
 			print(i)
+
+printPrime(10)
 
 # SieveOfEratosthenes
 # TimeComplexity---->O(n*log(log(n)))
@@ -21,10 +23,10 @@ def Sieve(n):
 	# Create a boolean array "prime[0..n]" and initialize all entries it as true
 	prime=[True for i in range(n+1)]
 	p=2
-	while(p*p<=n):
-		# If prime[p] is not changed, then it is a prime
+	for p in range(2,int(np.sqrt(n))+1):
+		# If prime[p] is True, then it is a prime
 		if (prime[p]==True):
-			# Update all multiples of p
+			# Update all multiples of p starting from its square
 			for i in range(p*p,n+1,p):
 				prime[i]=False
 		p+=1
@@ -35,9 +37,20 @@ def Sieve(n):
 			print(p)
 
 # To find LCM(smallest divisible numbers) of all N numbers with input N
-
 def lcm(N):
 	ans=1
 	for i in range(1,n+1):
 		ans=(int(ans*i)/math.gcd(ans,i))
 	return ans 
+
+# To find GCD using Euclid's Algorithm
+def gcd(a,b):
+	# if divisor becomes 0 then return the dividend
+	if b==0:
+		return a
+	# else recursively call function with 
+	# dividend = divisor
+	# divisor = remainder of a and b
+	return gcd(b,a%b)
+
+print(gcd(55,80))
